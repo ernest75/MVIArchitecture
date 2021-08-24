fc26fac8e7385a501edc5cad689a8cd9052fdf04
ernest75 <ernestschneider@gmail.com>
2021-08-24 18:57:39 +0200

    Added LiveDataCallAdapter.kt and factory to make retrofit able to work with LiveData type

--- app/src/main/java/com/example/mviarchitecture/api/ApiService.kt
+++ app/src/main/java/com/example/mviarchitecture/api/ApiService.kt
@@ -1,19 +1,21 @@
 package com.example.mviarchitecture.api
 
+import androidx.lifecycle.LiveData
 import com.example.mviarchitecture.model.BlogPost
 import com.example.mviarchitecture.model.User
+import com.example.mviarchitecture.util.GenericApiResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("placeholder/blogs")
-    fun getBlogs(): List<BlogPost>
+    fun getBlogs(): LiveData<GenericApiResponse<List<BlogPost>>>
 
     @GET("placeholder/user/{userID}")
     fun getUser(
         @Path("userId") userId: String
-    ) : User
+    ) : LiveData<GenericApiResponse<User>>
 
 
 }
